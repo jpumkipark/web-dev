fe6bb19656c0076507b16caa3a63cdcf489116eb
jpumki <jpumki96@gmail.com>
2021-10-19 12:10:42 -0400

    up

---
+++ src/components/a6/Practice/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoList from "./Todo/TodoList";
+const Practice = () => {
+  return (
+    <>
+      <h1>Practice</h1>
+      <ConditionalOutput />
+      <Styles />
+      <Classes />
+      <TodoList />
+    </>
+  );
+};
+
+export default Practice;
